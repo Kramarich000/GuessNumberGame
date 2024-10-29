9b46d23df385d6c845c50a660a17dbf697dca2f3
Karen <ipepela1@mail.ru~>
2024-10-29 21:55:47 +0300

    аутентификация добавлена

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Изменение в Program.cs
+
+using System;
+
+namespace GuessNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AuthService authService = new AuthService();
+            User user = authService.RegisterUser();
+
+            // Главный игровой процесс
+            // (поместить сюда вызов основной логики игры)
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,29 @@
+﻿// File: User.cs
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GuessNumberGame
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public List<int> Scores { get; set; } = new List<int>();
+
+        public void SaveScore(int score)
+        {
+            Scores.Add(score);
+            File.AppendAllText("scores.txt", $"{Username}: {score}\n");
+        }
+
+        public void DisplayScores()
+        {
+            Console.WriteLine($"Результаты игрока {Username}:");
+            foreach (var score in Scores)
+            {
+                Console.WriteLine(score);
+            }
+        }
+    }
+}
