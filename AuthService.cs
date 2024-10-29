ba832505fa5388d963cb7bb2449278897bf8f9ad
Karen <ipepela1@mail.ru~>
2024-10-29 22:20:42 +0300

    аутентификация

--- AuthService.cs
+++ AuthService.cs
@@ -1,16 +1,49 @@
-// File: AuthService.cs
-
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GuessNumberGame
 {
     public class AuthService
     {
-        public User RegisterUser()
+        private List<User> users = new List<User>();
+
+        public void Register(string username, string password)
         {
-            Console.Write("Введите имя пользователя: ");
-            string username = Console.ReadLine();
-            return new User { Username = username };
+            // Проверяем, существует ли пользователь
+            if (users.Any(u => u.Username == username))
+            {
+                Console.WriteLine("Пользователь с таким именем уже существует.");
+                return;
+            }
+
+            User newUser = new User(username, password);
+            users.Add(newUser);
+            Console.WriteLine($"Пользователь {username} зарегистрирован.");
+        }
+
+        public User Login(string username, string password)
+        {
+            var user = users.FirstOrDefault(u => u.Username == username);
+            if (user != null && user.ValidatePassword(password))
+            {
+                Console.WriteLine($"Пользователь {username} вошел в систему.");
+                return user;
+            }
+            else
+            {
+                Console.WriteLine("Неверное имя пользователя или пароль.");
+                return null;
+            }
+        }
+
+        public void DisplayAllUsers()
+        {
+            Console.WriteLine("Список пользователей:");
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.Username);
+            }
         }
     }
 }
