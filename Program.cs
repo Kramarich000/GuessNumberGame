2bc354ea0fee983e59a5c47d8d2afea1442e40a3
Karen <ipepela1@mail.ru~>
2024-10-29 21:53:15 +0300

    игруха

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// File: Program.cs
+
+using System;
+
+namespace GuessNumberGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать в игру 'Угадай число'!");
+            Console.WriteLine("Выберите уровень сложности: 1 - Легкий, 2 - Средний, 3 - Сложный");
+            int difficulty = int.Parse(Console.ReadLine());
+            int maxNumber = difficulty switch
+            {
+                1 => 10,
+                2 => 50,
+                3 => 100,
+                _ => 10
+            };
+
+            Random random = new Random();
+            int secretNumber = random.Next(1, maxNumber + 1);
+            int attempts = 0;
+            bool guessed = false;
+
+            Console.WriteLine($"Я загадал число от 1 до {maxNumber}. Попробуйте угадать!");
+
+            while (!guessed)
+            {
+                Console.Write("Введите ваше число: ");
+                int userGuess = int.Parse(Console.ReadLine());
+                attempts++;
+
+                if (userGuess < secretNumber)
+                {
+                    Console.WriteLine("Слишком низкое число. Попробуйте снова.");
+                }
+                else if (userGuess > secretNumber)
+                {
+                    Console.WriteLine("Слишком высокое число. Попробуйте снова.");
+                }
+                else
+                {
+                    guessed = true;
+                    Console.WriteLine($"Поздравляем! Вы угадали число {secretNumber} за {attempts} попыток.");
+                }
+            }
+        }
+    }
+}
