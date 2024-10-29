2a9867adfd6c6c9f660adc0a61f2555fbd08f342
Karen <ipepela1@mail.ru~>
2024-10-29 22:29:52 +0300

    ... еще раз

--- obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff1ad11d6544e1ffb502dcd9bebad814be7d869")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cc839f4a74c284eed2543f42f67189014a4f610")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
