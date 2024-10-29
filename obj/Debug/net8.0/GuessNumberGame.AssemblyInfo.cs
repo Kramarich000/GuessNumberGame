5ff1ad11d6544e1ffb502dcd9bebad814be7d869
Karen <ipepela1@mail.ru~>
2024-10-29 22:25:17 +0300

    добавил в гитигнор мусор

--- obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a63b53228b69fd69d79413dbe7f6cf828d1fd8e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eefbc08c2e5ec5ca230a004d5e57046ef3c9338")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
