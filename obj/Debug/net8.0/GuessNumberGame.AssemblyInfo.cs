9cc839f4a74c284eed2543f42f67189014a4f610
Karen <ipepela1@mail.ru~>
2024-10-29 22:28:26 +0300

    обновил гитинор АААААА КЭЭШ

--- obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/GuessNumberGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5eefbc08c2e5ec5ca230a004d5e57046ef3c9338")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ff1ad11d6544e1ffb502dcd9bebad814be7d869")]
 [assembly: System.Reflection.AssemblyProductAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GuessNumberGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
